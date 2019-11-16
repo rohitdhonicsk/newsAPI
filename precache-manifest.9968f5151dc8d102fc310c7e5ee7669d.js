06ff9e404b53a925e93b103e781fffb2f418d914
rohitdhonicsk <rohitgupta.dbs@gmail.com>
2019-11-16 15:31:15 +0530

    Updates

---
+++ precache-manifest.9968f5151dc8d102fc310c7e5ee7669d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1f7d2fd31542ae3872a13b80357bf4",
+    "url": "/newsAPI/index.html"
+  },
+  {
+    "revision": "682f7afcb5f6169158dc",
+    "url": "/newsAPI/static/css/main.3283374f.chunk.css"
+  },
+  {
+    "revision": "a5130752da3c11344199",
+    "url": "/newsAPI/static/js/2.e7938268.chunk.js"
+  },
+  {
+    "revision": "682f7afcb5f6169158dc",
+    "url": "/newsAPI/static/js/main.70821671.chunk.js"
+  },
+  {
+    "revision": "3bc291980a78d377174c",
+    "url": "/newsAPI/static/js/runtime-main.9b47fc21.js"
+  }
+]);
